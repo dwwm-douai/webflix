18365a8a9089600ceb00d9db1452c517708fd575
Matthieu Mota <matthieumota@gmail.com>
2022-02-22 12:11:09 +0100

    Appel API tmdb

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Models\Movie;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,7 +19,18 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
 
-        Category::factory(30)->create();
+        // On va faire un appel sur l'API de The Movie DB
+        $response = Http::get('https://api.themoviedb.org/3/genre/movie/list?api_key=ebc0a4ad59da5f80113ec7d1142c72a7&language=fr');
+        $genres = $response->json()['genres'];
+
+        foreach ($genres as $genre) {
+            Category::factory()->create(['name' => $genre['name']]);
+        }
+
+        // Category::factory(30)->create();
+        
+        // .......
+        
         Movie::factory(30)->create();
     }
 }
